e85b1b77766bd026ab7196a0cd383ee7475bd335
Andrea-Paperini <etabeta99@live.it>
2019-11-25 16:00:55 +0100

    Finito

---
+++ main.js
@@ -0,0 +1,2 @@
+$(".hamburger-menu").addClass("active");
+// Seleziono per la classe del hamburger menu la classe active che contiene il display block all'interno della media query per visualizzarlo in automatico solo in versione mobile
