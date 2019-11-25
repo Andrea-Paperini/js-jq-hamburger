aca15a0a0084795ce1122fc3648d45073b988387
Andrea-Paperini <etabeta99@live.it>
2019-11-25 20:08:21 +0100

    avevo dimenticato il click, finito

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-$(".hamburger-menu").addClass("active");
-// Seleziono per la classe del hamburger menu la classe active che contiene il display block all'interno della media query per visualizzarlo in automatico solo in versione mobile
+$(".header-right > a").click(function(){
+  $(".hamburger-menu").addClass("active");
+})
+// Seleziono per la classe del hamburger menu la classe active che contiene il display block all'interno della media query per visualizzarlo in automatico solo in versione mobile sul click dell'utente
+
+$(".hamburger-menu > a").click(function(){
+  $(".hamburger-menu ").removeClass("active");
+})
+// Rimuovo la classe che mantiene la visualizzazione su click dell'utente
